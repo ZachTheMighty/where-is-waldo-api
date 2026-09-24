16de34425c8ae77132c79220a3d1928eaa85cdbe
ZachTheMighty <promoted66@gmail.com>
2026-09-24 15:50:01 +0300

    fix: coords ids increment with each deployment
    since the client expects fixed coords ids (1, 2, 3, 4), having the ids autoincrement means the client will never find these coords. Fixed by using hardcoded ids and creating them only when they don't already exist using upsert

--- populated.js
+++ populated.js
@@ -1,40 +1,20 @@
 const prisma = require("./lib/prisma.ts");
 
 async function main() {
-  await prisma.coord.deleteMany({});
+  const coords = [
+    { id: 1, minX: 25, maxX: 27, minY: 34, maxY: 39 },
+    { id: 2, minX: 60, maxX: 61, minY: 38, maxY: 41 },
+    { id: 3, minX: 2, maxX: 5, minY: 42, maxY: 45 },
+    { id: 4, minX: 63, maxX: 64, minY: 47, maxY: 50 },
+  ];
 
-  await prisma.coord.create({
-    data: {
-      minX: 25,
-      maxX: 27,
-      minY: 34,
-      maxY: 39,
-    },
-  });
-  await prisma.coord.create({
-    data: {
-      minX: 60,
-      maxX: 61,
-      minY: 38,
-      maxY: 41,
-    },
-  });
-  await prisma.coord.create({
-    data: {
-      minX: 2,
-      maxX: 5,
-      minY: 42,
-      maxY: 45,
-    },
-  });
-  await prisma.coord.create({
-    data: {
-      minX: 63,
-      maxX: 64,
-      minY: 47,
-      maxY: 50,
-    },
-  });
+  for (const coord of coords) {
+    await prisma.coord.upsert({
+      where: { id: coord.id },
+      update: {},
+      create: coord,
+    });
+  }
 }
 
 main()
